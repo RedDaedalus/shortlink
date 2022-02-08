92627156670d001151bcc5b5a4d776128681ecff
Daedalus <16168171+RedDaedalus@users.noreply.github.com>
2022-02-08 09:11:46 -0700

    Implement link lookup

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,11 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const slug = new URL(request.url).pathname.substring(1);
+  console.log(`Looking up url for ${slug}`);
+  
+  const url = await SHORTLINK_URLS.get(slug);
+  if (!url) {
+    return new Response("Not Found", { status: 404 });
+  }
+
+  return Response.redirect(url, 301);
 }
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+declare global {
+    const SHORTLINK_URLS: KVNamespace;
+}
+
+export {}
